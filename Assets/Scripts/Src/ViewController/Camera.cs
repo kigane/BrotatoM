e286c1d1e3e0a4fe0ef4d1a6b2ae29fa48368f65
kigane <huwkigane@outlook.com>
2022-11-29 18:55:45 +0800

    添加玩家控制和相机控制

---
+++ Assets/Scripts/Src/ViewController/Camera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace BrotatoM
+{
+    public class Camera : MonoBehaviour
+    {
+        private void LateUpdate()
+        {
+            var playerGO = GameObject.FindWithTag("Player");
+            if (!playerGO)
+                return;
+
+            Transform playerTransform = playerGO.transform;
+
+            var camPos = playerTransform.position;
+            camPos.x = Mathf.Clamp(camPos.x, -6, 6);
+            camPos.y = Mathf.Clamp(camPos.y, -4, 4);
+            camPos.z = transform.position.z;
+
+            transform.position = camPos;
+        }
+    }
+}
---
+++ Assets/Scripts/Src/ViewController/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace BrotatoM
+{
+    public class Player : MonoBehaviour
+    {
+        public float moveSpeed;
+        private PlayerControl mPlayerControl;
+
+        private void Awake()
+        {
+            mPlayerControl = new PlayerControl();
+        }
+
+        private void Update()
+        {
+            var moveSignal = mPlayerControl.Player.Move.ReadValue<Vector2>();
+            var currPos = transform.position;
+            currPos.x += moveSignal.x * Time.deltaTime * moveSpeed;
+            currPos.x = Mathf.Clamp(currPos.x, -14, 14);
+            currPos.y += moveSignal.y * Time.deltaTime * moveSpeed;
+            currPos.y = Mathf.Clamp(currPos.y, -8, 8);
+            transform.position = currPos;
+        }
+
+        private void OnEnable()
+        {
+            mPlayerControl.Enable();
+        }
+
+        private void OnDisable()
+        {
+            mPlayerControl.Disable();
+        }
+    }
+}
