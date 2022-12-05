c56e20484b167aed261fa16f7338868e851bfd7e
kigane <huwkigane@outlook.com>
2022-12-05 12:58:20 +0800

    添加简单的日志类

---
+++ Assets/Scripts/Src/Utilities/Log.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace BrotatoM
+{
+    public class Log
+    {
+        [Conditional("Debug")]
+        public static void Debug(string message)
+        {
+            UnityEngine.Debug.Log("<color=cyan>" + message + "</color>");
+        }
+
+        [Conditional("Debug"), Conditional("Info")]
+        public static void Info(string message)
+        {
+            UnityEngine.Debug.Log("<color=green>" + message + "</color>");
+        }
+
+        [Conditional("Debug"), Conditional("Info"), Conditional("Warning")]
+        public static void Warning(string message)
+        {
+            UnityEngine.Debug.LogWarning("<color=yellow>" + message + "</color>");
+        }
+
+        [Conditional("Debug"), Conditional("Info"), Conditional("Warning"), Conditional("Error")]
+        public static void Error(string message)
+        {
+            UnityEngine.Debug.LogError("<color=red>" + message + "</color>");
+        }
+    }
+}
