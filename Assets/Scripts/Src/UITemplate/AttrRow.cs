90aecedabcfc5bcb99054fa6420cc92a4df7e0ec
kigane <huwkigane@outlook.com>
2022-12-06 00:20:50 +0800

    fix typo

--- Assets/Scripts/Src/UITemplate/AttrRow.cs
+++ Assets/Scripts/Src/UITemplate/AttrRow.cs
@@ -22,7 +22,7 @@ namespace BrotatoM
 
         public AttrRow(AttrInfo attrInfo) : this()
         {
-            mTemplateContainer.Q("attr-small-icon").style.backgroundImage = new StyleBackground(Resources.Load<Sprite>(attrInfo.path));
+            mTemplateContainer.Q("attr-icon").style.backgroundImage = new StyleBackground(Resources.Load<Sprite>(attrInfo.path));
             mTemplateContainer.Q<Label>("attr-name-text").text = attrInfo.name;
             mTemplateContainer.Q<Label>("attr-value-text").text = attrInfo.value.ToString();
         }
